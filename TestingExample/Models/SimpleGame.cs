3c62ab891291cd42637f9a70704c7b9dcebeacfb
evanjkelly123@gmail.com <evanjkelly123@gmail.com>
2019-10-21 13:36:15 -0700

    Add project files.

---
+++ TestingExample/Models/SimpleGame.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingExample.Models
+{
+    /// <summary>
+    /// SimpleGame tracks four optional game scores
+    /// from 0 - 300 and a total score
+    /// </summary>
+    public class SimpleGame
+    {
+        private int? game1;
+        private int? game2;
+
+        
+        public int? Game1 {
+            get { 
+                return game1; 
+            }
+            set
+            {
+                ValidateGameScore(value, out game1);
+                game1 = value;
+            }
+        }
+
+        private void ValidateGameScore(int? s, out int? game)
+        {
+            if (!s.HasValue || s < 0 || s > 300)
+            {
+                throw new ArgumentOutOfRangeException
+                    ("Scores must be between 0 and 300 inclusive");
+            }
+            game = s.Value;
+        }
+
+        public int? Game2 { get => game2;
+            set
+            {
+                ValidateGameScore(value, out game2);
+                game2 = value;
+            }
+        }
+
+        public int? Game3 { get; set; }
+
+        public int? Game4 { get; set; }
+
+        public int TotalScore
+        {
+            get
+            {
+                return Game1.GetValueOrDefault() + Game2.GetValueOrDefault() +
+                  Game3.GetValueOrDefault() + Game4.GetValueOrDefault();
+            }
+        }
+
+
+    }
+}
---
+++ TestingExampleTests/Models/StringHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestingExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestingExample.Models.Tests
+{
+    [TestClass()]
+    
+    public class StringHelperTests
+    {
+        [TestMethod()]
+        [DataRow("joe.txt")]
+        [DataRow("test.TXT")]
+        [DataRow("   test.txt")]
+        [DataRow("test.txt   ")]
+        [DataRow("   test.txt   ")]
+        [DataRow("October2019.TXT")]
+        [DataRow("null.text")]
+        
+        public void IstTextFile_ValidFileName_ReturnsTrue(string fileName)
+        {
+            
+            
+            //Act
+           bool result = Stringhelper.IsTextFile(fileName);
+
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow(".text")]
+        [DataRow("hello world")]
+        [DataRow(" .txt")]
+        [DataRow("<>.txt")]
+        public void IsTextFile_InvalidFileName_ReturnsFalse(string fileName)
+        {
+   
+            bool result = Stringhelper.IsTextFile(fileName);
+
+            Assert.IsFalse(result);
+        }
+    }
+}
